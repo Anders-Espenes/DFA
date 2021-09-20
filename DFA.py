bf53a64f394466d9977fbaeadc90a49095f7d4f6
Martinius Feragen <andrmf18@uia.no>
2021-09-20 18:25:31 +0200

    DFA can now build APTA tree using nodes

--- DFA.py
+++ DFA.py
@@ -1,4 +1,5 @@
 import itertools as it
+from node import Node
 
 class DFA:
 	def __init__(self, trans, start: int, accepting, alphabet):
@@ -17,20 +18,40 @@ class DFA:
 	def generate_all_strings(self, length: int, p: bool = False):
 		string_results = []
 
-		all_string = it.product(self.alphabet, repeat=length)
+		for i in range(0, length+1):
+			all_string = it.product(self.alphabet, repeat=i)
 
-		# Handling the first string, which is empty
-		all_string.__next__()
-		if p:
-			print(f"Creating all string from alphabet, {self.alphabet}, up to length {length}")
-			print(f"String: e, result: {self.input()}")
-		string_results.append(["e", self.input()])
-
-		# Handles all string up to length
-		for s in all_string:
-			string = "".join(map(str, s)).lstrip("0")
-			string_results.append([string, self.input(string)])
 			if p:
-				print(f"String: {string}, result: {self.input(string)}")
+				print(f"Creating all string from alphabet, {self.alphabet}, up to length {i}")
+
+			string_results.append([])
+
+			# Handles all string up to length
+			for s in all_string:
+				string = "".join(map(str, s))
+				string_results[i].append([string, self.input(string)])
+				if p:
+					print(f"String: {string}, result: {self.input(string)}")
 
 		return string_results
+
+	def build_APTA(self, length: int):
+		all_strings = self.generate_all_strings(length)
+
+		# removed the first element that is the empty string
+		# Pop pop since its a List[List[]]
+		root_string = all_strings.pop(0).pop(0)
+
+		root_node = Node(value=root_string[0], data="e", accepting=root_string[1])
+
+		# adding nodes in breadth first
+		for level in all_strings:
+			for nodes in level:
+				current_node = root_node
+				for trans in nodes[0]:
+					next_node = current_node.goto(trans)
+					if next_node is None:
+						current_node.setChild(Node(value=trans, data=nodes[0], accepting=nodes[1]))
+					else:
+						current_node = next_node
+		root_node.print_nodes()
--- node.py
+++ node.py
@@ -1,12 +1,32 @@
-class Node:
-    def __init__(self):
-        self.left = None
-        self.right = None
-    
-    def setLeftChild(self, child):
-        self.left = child
-    
-    def setRightChild(self, child):
-        self.right = child
+from typing import Optional, Type
 
-    
+class Node:
+    def __init__(self, value, data, accepting):
+        self.value = value
+        self.data = data
+        self.accepting: bool = accepting
+        self.children: list[Node] = []
+
+    def setChild(self, child):
+        self.children.append(child)
+
+    def removeChild(self, child):
+        self.children.remove(child)
+
+    def goto(self, value):
+        for child in self.children:
+            if child.value == value:
+                return child
+        else:
+            return None
+
+    def print_nodes(self):
+        """Prints node and all child nodes in depth first order"""
+        print(self)
+        for child in self.children:
+            child.print_nodes()
+
+    def __repr__(self):
+        return f"Data: {self.data}, acc: {self.accepting}"
+
+
