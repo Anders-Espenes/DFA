b517e06a4486d36e15c627704064165f393655e6
Martinius Feragen <andrmf18@uia.no>
2021-09-21 18:46:01 +0200

    test code for matching and merging nodes

--- main.py
+++ main.py
@@ -33,7 +33,50 @@ def generateStrings():
 	)
 
 
-	dfa_8.build_APTA(10)
+	# dfa_8.build_prefix_tree(3)
+
+	root_node = Node("", "e", False)
+
+	node0 = Node("0", "0", False)
+	node1 = Node("1", "1", True)
+
+	node00 = Node("0", "00", False)
+	node01 = Node("1", "01", True)
+	node10 = Node("0", "10", True)
+	node11 = Node("1", "11", False)
+
+	node000 = Node("0", "000", False)
+	node001 = Node("1", "001", True)
+	node010 = Node("0", "010", True)
+	node011 = Node("1", "011", False)
+	node111 = Node("1", "111", True)
+	node110 = Node("0", "110", False)
+	node101 = Node("1", "101", False)
+	node100 = Node("0", "100", True)
+
+	root_node.setChild(node0)
+	root_node.setChild(node1)
+
+	node0.setChild(node00)
+	node0.setChild(node01)
+	node1.setChild(node10)
+	node1.setChild(node11)
+
+	node00.setChild(node000)
+	node00.setChild(node001)
+	node01.setChild(node010)
+	node01.setChild(node011)
+	node10.setChild(node100)
+	node10.setChild(node101)
+	node11.setChild(node110)
+	node11.setChild(node111)
+
+
+	dfa_10.match_labels(root_node)
+
+
+
+
 
 
 def greedy():
--- node.py
+++ node.py
@@ -1,32 +1,31 @@
 from typing import Optional, Type
 
+
 class Node:
-    def __init__(self, value, data, accepting):
-        self.value = value
-        self.data = data
-        self.accepting: bool = accepting
-        self.children: list[Node] = []
+	def __init__(self, value, data, accepting):
+		self.value = value
+		self.data = data
+		self.accepting: bool = accepting
+		self.children: list[Node] = []
 
-    def setChild(self, child):
-        self.children.append(child)
+	def setChild(self, child):
+		self.children.append(child)
 
-    def removeChild(self, child):
-        self.children.remove(child)
+	def removeChild(self, child):
+		self.children.remove(child)
 
-    def goto(self, value):
-        for child in self.children:
-            if child.value == value:
-                return child
-        else:
-            return None
+	def transition(self, value):
+		for child in self.children:
+			if child.value == value:
+				return child
+		else:
+			return None
 
-    def print_nodes(self):
-        """Prints node and all child nodes in depth first order"""
-        print(self)
-        for child in self.children:
-            child.print_nodes()
+	def print_nodes(self):
+		"""Prints node and all child nodes in depth first order"""
+		print(self)
+		for child in self.children:
+			child.print_nodes()
 
-    def __repr__(self):
-        return f"Data: {self.data}, acc: {self.accepting}"
-
-
+	def __repr__(self):
+		return f"Data: {self.data}, acc: {self.accepting}"
